072353831e2272aae3ba2b199fe8ed8f24977b67
wildflower <wildflower1975@gmail.com>
2014-03-17 22:43:02 +1300

    Create CourseDateTime.php

---
+++ CourseDateTime.php
@@ -0,0 +1,33 @@
+<?php
+class Course extends CalendarEvent {
+	static $db = array (
+		'Cost' => 'Currency'
+	);
+
+    static $has_many = array (
+
+        'DateTimes' => 'CourseDateTime'
+
+    );
+
+	static $has_one = array (
+		'CourseHolder' => 'CourseHolder'
+		);
+		
+	static $can_be_root = false;
+	
+	static $hide_ancestor = 'CalendarEvent';
+	
+    public function getCMSFields(){
+	$f = parent::getCMSFields();
+	$f->addFieldTotab("root.Content.main", new 
+	CurrencyField('Cost',_t('Course.COST','Course Cost')),'Content');
+	return $f;
+    
+    }
+ 
+}
+class Course_Controller extends CalendarEvent_Controller {
+
+}
+?>
